a04347c8187e08a75e55015d0f7c2892f1e971b7
Esteban Degetau <estebandegetau@gmail.com>
2025-07-09 12:26:19 -0600

    Series reviewed

--- R/read_sheets.R
+++ R/read_sheets.R
@@ -72,55 +72,118 @@ read_sheet <- function(path, position) {
       date = as.Date(as.numeric(date), origin = "1899-12-30") |>
         as_date()
     ) |>
-    janitor::clean_names()
+    janitor::clean_names() |>
+    drop_na()
 
   return(a)
 }
 
-#---- Load ---------------------------------------------------------------------
+#---- Explore ---------------------------------------------------------------------
 
-dirs <- "//11.254.31.147/dasr" |>
+dirs <- here("data/raw") |>
   list.files(
-    pattern = "ASEGURADOS|PATRONES|SALARIO|MASA|TRABSAL",
+   pattern = ".xlsx",
     recursive = T,
     full.names = T
   )
 
-
-
-books <- tibble(
+catalogue <- tibble(
   path = dirs
 ) |>
   filter(
-    !str_detect(path, "\\$")
+    str_detect(path, "ASEGURADOS TRABAJADORES|PATRONES|SALARIO")
   ) |>
   mutate(
-    content = map(path, read_content),
-    positions = map(path, pull_poistions),
+    # sheets = map(path, excel_sheets),
     name = map_chr(path, ~ str_extract(.x, "([^/]+)\\.xlsx$")) |>
       str_remove("\\.xlsx"),
     level = case_when(
       str_detect(name, "Delegaciones") ~ "Delegación",
-      str_detect(name, "Subdelegaciones") ~ "Subdelegación"
+      str_detect(name, "Subdelegaciones") ~ "Subdelegación",
+      str_detect(path, "DELEGACIONES") ~ "Delegación",
+      str_detect(path, "SUBDELEGACIONES") ~ "Subdelegación"
     ),
     name = name |>
       str_remove("_\\w+") |>
       str_squish() |>
       str_remove("\\.") |>
+      # Remove accents
+      iconv(to = "ASCII//TRANSLIT") |>
       str_to_lower(),
     series = case_when(
-      str_detect(name, "asegurados") ~ "Asegurados Trabajadores",
+      
       str_detect(name, "patrones") ~ "Patrones",
       str_detect(name, "masa") ~ "Masa salarial",
       str_detect(name, "salario") ~ "Salario",
-      str_detect(name, "trabsal") ~ "Trabsal"
+      str_detect(name, "trabsal") ~ "Trabsal",
+      str_detect(name, "asegurados trabajadores") ~ "Asegurados trabajadores",
+      str_detect(name, "ema") ~ "EMA",
+      str_detect(name, "ibc") ~ "IBC",
+      str_detect(name, "inc") ~ "INC",
+      str_detect(name, "metas") ~ "Metas",
+      str_detect(name, "mora") ~ "Mora",
+      str_detect(name, "recaudacion") ~ "Recaudación",
+      str_detect(name, "consultorio") ~ "Población derechohabiente adscrita a consultorio",
+      str_detect(name, "adscrita|pda") ~ "Población derechohabiente adscrita",
+      str_detect(name, "potencial") ~ "Población derechohabiente potencial",
+      str_detect(name, "derechohabiente") ~ "Población derechohabiente",
     ),
     disaggregation = case_when(
       str_detect(name, "modalidad") ~ "Modalidad",
       str_detect(name, "sector") ~ "Sector económico",
       str_detect(name, "rango") ~ "Rango de trabajadores",
-      T ~ "Modalidad"
-    )
+      str_detect(name, "umf") ~ "UMF",
+      str_detect(path, "ASEGURADOS") ~ "Modalidad"
+    ),
+    # category = case_when(
+    #   str_detect(name, "asegurados") ~ "Asegurados Trabajadores",
+    #   str_detect(name, "patrones") ~ "Patrones",
+    #   str_detect(name, "masa") ~ "Masa salarial",
+    #   str_detect(name, "salario") ~ "Salario",
+    #   str_detect(name, "trabsal") ~ "Trabsal",
+    #   str_detect(path, "METAS") ~ "Metas",
+    #   str_detect(path, "POBLACIONES") ~ "Poblaciones"
+    # )
+  ) |>
+  filter(
+    !str_detect(name, "rango salarial")
+  )
+
+# Write catalogue to xlsx
+catalogue |> writexl::write_xlsx(here("data/catalogue.xlsx"))
+catalogue |> view()
+
+
+#---- Read content and positions ------------------------------------------------
+
+books <- catalogue |>
+  mutate(
+    content = map(path, read_content),
+    positions = map(path, pull_poistions),
+    # name = map_chr(path, ~ str_extract(.x, "([^/]+)\\.xlsx$")) |>
+    #   str_remove("\\.xlsx"),
+    # level = case_when(
+    #   str_detect(name, "Delegaciones") ~ "Delegación",
+    #   str_detect(name, "Subdelegaciones") ~ "Subdelegación"
+    # ),
+    # name = name |>
+    #   str_remove("_\\w+") |>
+    #   str_squish() |>
+    #   str_remove("\\.") |>
+    #   str_to_lower(),
+    # series = case_when(
+    #   str_detect(name, "asegurados trabajadores") ~ "Asegurados Trabajadores",
+    #   str_detect(name, "patrones") ~ "Patrones",
+    #   str_detect(name, "masa") ~ "Masa salarial",
+    #   str_detect(name, "salario") ~ "Salario",
+    #   str_detect(name, "trabsal") ~ "Trabsal"
+    # ),
+    # disaggregation = case_when(
+    #   str_detect(name, "modalidad") ~ "Modalidad",
+    #   str_detect(name, "sector") ~ "Sector económico",
+    #   str_detect(name, "rango") ~ "Rango de trabajadores",
+    #   T ~ "Modalidad"
+    # )
   )
 
 
@@ -156,17 +219,25 @@ sheets_raw <- books_indexed |>
 
 
 fix_dates <- sheets_raw |>
-  mutate(dates_errors = map_dbl(data, ~ .x |>
-    filter(is.na(date)) |>
-    nrow())) |>
+  mutate(
+    dates_errors = map_dbl(
+      data,
+      ~ .x |>
+        filter(is.na(date)) |>
+        nrow()
+    )
+  ) |>
   filter(dates_errors > 0) |>
   mutate(
-    data = map(data, ~ .x |>
-      mutate(
-        date_c = seq(first(date), along.with = date, by = "month"),
-        date = date_c
-      ) |>
-      select(!date_c))
+    data = map(
+      data,
+      ~ .x |>
+        mutate(
+          date_c = seq(first(date), along.with = date, by = "month"),
+          date = date_c
+        ) |>
+        select(!date_c)
+    )
   )
 
 sheets_clean <- sheets_raw |>
@@ -186,18 +257,37 @@ sheets_clean <- sheets_raw |>
   ) |>
   mutate(
     across(matches("_name"), str_to_sentence),
-    data = map(data,
-     ~ .x |>
-        rename_with( ~ .x |> 
-          str_replace("clave_del$", "clave_delegacion")
-          )
+    data = map(
+      data,
+      ~ .x |>
+        rename_with(
+          ~ .x |>
+            str_replace("clave_del$", "clave_delegacion")
+        )
     )
-    ) 
+  ) |>
+  filter(!str_detect(book_name, "Trabsal|Masa")) |>
+  filter(!str_detect(sheet_name, "total|Total")) |>
+  filter(
+    (disaggregation == "Modalidad" & str_detect(sheet_name, "\\d")) |
+      disaggregation != "Modalidad"
+  ) |>
+  mutate(
+    book_name = case_when(
+      series == "Salario" ~ "Salario trabajadores",
+      T ~ book_name
+    )
+  )
 
-sheets_raw$data[[118]]
+if (0) {sheets_raw$data[[118]]
+
+sheets_raw$data[[118]] |> filter(date == max(date)) |> view()
+
 sheets_raw |> slice(118) |> glimpse()
-glimpse(sheets_clean)
 
+
+glimpse(sheets_clean)
+}
 #---- Save -----------------------------------------------------------
 
 sheets <- sheets_clean
@@ -207,10 +297,24 @@ save(sheets, file = here("data/sheets.RData"))
 
 series <- sheets |> 
   unnest(data) |>
-  relocate(clave_delegacion, delegacion, clave_subdelegacion, subdelegacion, date, value, .after = disaggregation)
+  relocate(clave_delegacion, delegacion, clave_subdelegacion, subdelegacion, date, value, .after = disaggregation) |>
+  mutate(
+    date = as_date(date),
+    delegacion = case_when(
+      str_detect(delegacion, "TOTAL|Total") ~ "Nacional",
+      T ~ delegacion
+    )
+  
+  ) 
+
+series |> 
+  distinct(series, disaggregation, sheet_name) |>
+  collect() |>
+  arrange(series, disaggregation, sheet_name) |>
+  write_csv(file = here("data/labels.csv"))
 
 series |>
-  group_by(book_name) |>
+  group_by(series, disaggregation) |>
   arrow::write_dataset(
     path = here("data/sheets"),
     format = "parquet"
